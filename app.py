942305fc19ec99c10a441fbbc549e01712454e96
Praneesh Sharma <92104015+Praneesh-Sharma@users.noreply.github.com>
2023-06-15 15:25:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,28 +10,17 @@ st.title('Caustic Conusmption Prediction')
 st.markdown("<h2 style='text-align: left; color: black;'>Model Parameters</h2>", unsafe_allow_html=True)
 st.write("###### Specify Input Parameters: ")
 
-def user_input():
+st.header("Feed Rates:")
+col1, col2 = st.columns(2)
+
+with col1:
     isom = st.slider('ISOM Feed Rate', 0.0, 45.0, 1.0)
     nfccu = st.slider('NFCCU Feed Rate', 0.0, 250.0, 150.0)
+
+with col2:
     ofccu = st.slider('OFCCU Feed Rate', 0.0, 200.0,  50.0)
     ltu = st.slider('LTU Feed Rate', 0.0, 150.0,  50.0)
-    data = { 'ISOM': isom,
-             'NFCCU': nfccu,
-             'OFCCU': ofccu,
-             'LTU': ltu}
-    features = pd.DataFrame(data, index=[0])
-    return features
-
-df = user_input()
-
-#Displaying given input parameters
-st.write("###### Specified Input Parameters")
-st.write(df)
-st.write('---')
-
-#model
-prediction = predict(df.to_numpy())
 
 st.write("###### Predicted House Price Value based on the given parameters: ")
-st.code(float(prediction))
-st.write('---')
+result = predict(np.array([[isom, nfccu, ofccu, ltu]]))
+st.text(result[0])
