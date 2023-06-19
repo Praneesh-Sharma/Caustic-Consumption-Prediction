7da92aa5fd30ec4a586dd9532518d8b2fa2b215a
Praneesh Sharma <92104015+Praneesh-Sharma@users.noreply.github.com>
2023-06-19 16:04:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from prediction import predict
+from prediction import predicts
 
 st.title('Caustic Conusmption Prediction')
 
@@ -11,17 +12,20 @@ st.markdown("<h2 style='text-align: left; color: black;'>Model Parameters</h2>",
 st.write("###### Specify Input Parameters: ")
 
 st.header("Feed Rates:")
-col1, col2 = st.columns(2)
 
-with col1:
+nfccu = st.slider('NFCCU Feed Rate', 0.0, 250.0, 150.0)
+ofccu = st.slider('OFCCU Feed Rate', 0.0, 200.0,  50.0)
+ltu = st.slider('LTU Feed Rate', 0.0, 150.0,  50.0)  
+
+isom_chk = st.checkbox('Include ISOM Feed Rate')
+if isom_chk:
     isom = st.slider('ISOM Feed Rate', 0.0, 45.0, 1.0)
-    nfccu = st.slider('NFCCU Feed Rate', 0.0, 250.0, 150.0)
-
-with col2:
-    ofccu = st.slider('OFCCU Feed Rate', 0.0, 200.0,  50.0)
-    ltu = st.slider('LTU Feed Rate', 0.0, 150.0,  50.0)
-
-st.text('')
-st.write("#### Consumption: ")
-result = predict(np.array([[isom, nfccu, ofccu, ltu]]))
-st.text(result[0])
+    st.text('')
+    st.write("#### Consumption: ")
+    result = predict(np.array([[isom, nfccu, ofccu, ltu]]))
+    st.text(result[0])
+else:
+    st.text('')
+    st.write("#### Consumption: ")
+    result = predicts(np.array([[nfccu, ofccu, ltu]]))
+    st.text(result[0])
