7f969150b274cd1b40d44444e499f39febd68d1b
Praneesh Sharma <92104015+Praneesh-Sharma@users.noreply.github.com>
2023-06-19 15:59:02 +0530

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -3,3 +3,7 @@ import joblib
 def predict(data):
     clf = joblib.load("rf_model.sav")
     return clf.predict(data)
+
+def predicts(data):
+    clf = joblib.load("rf_model_v2.sav")
+    return clf.predict(data)
