5ce764b60c6e54a019a76781d755c72de66a1d41
Praneesh Sharma <92104015+Praneesh-Sharma@users.noreply.github.com>
2023-06-20 13:24:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ st.title('Caustic Conusmption Prediction')
 
 #User input for model parameters
 st.markdown("<h2 style='text-align: left; color: black;'>Model Parameters</h2>", unsafe_allow_html=True)
-st.write("###### Specify Input Parameters: ")
 
 nfccu = st.slider('NFCCU Feed Rate', 0.0, 250.0, 150.0)
 ofccu = st.slider('OFCCU Feed Rate', 0.0, 200.0,  50.0)
