9f929731e617af911161ae8db8c433817eb5d817
Praneesh Sharma <92104015+Praneesh-Sharma@users.noreply.github.com>
2023-06-21 14:53:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,8 +21,10 @@ result = predict(np.array([[nfccu, ofccu, ltu]]))
 isom_chk = st.checkbox('Include ISOM Feed Rate')
 if isom_chk:
     isom = st.number_input('Enter ISOM Consumption: ')
+    st.write('---')
     st.text("Predicted output:")
     st.code(result[0]+isom)
 else:
+    st.write('---')
     st.text("Predicted output:")
     st.code(result[0])    
