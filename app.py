e723d6fdd95dca33d646ba724eb0540bb65809e3
Praneesh Sharma <92104015+Praneesh-Sharma@users.noreply.github.com>
2023-06-21 15:57:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ import pandas as pd
 import numpy as np
 from prediction import predict
 
-st.title('Caustic Conusmption Prediction')
+st.title('Caustic Consumption Prediction')
 
 #User input for model parameters
-st.markdown("<h2 style='text-align: left; color: black;'>Model Parameters</h2>", unsafe_allow_html=True)
+st.markdown("<h3 style='text-align: left; color: black;'>Model Parameters</h3>", unsafe_allow_html=True)
 
 nfccu = st.slider('NFCCU Feed Rate', 0.0, 220.0, 50.0)
 ofccu = st.slider('OFCCU Feed Rate', 0.0, 120.0,  50.0)
@@ -15,16 +15,16 @@ ltu = st.slider('LTU Feed Rate', 0.0, 110.0,  50.0)
 
 if nfccu>80 and ofccu>80 and ltu>80:
     result = predict(np.array([[nfccu, ofccu, ltu]]))
-    isom_chk = st.checkbox('Include ISOM Feed Rate')
+    isom_chk = st.checkbox('Include ISOM consumption')
     if isom_chk:
         isom = st.number_input('Enter ISOM Consumption: ')
         st.write('---')
         st.text("Predicted output:")
-        st.code(result[0]+isom)
+        st.code(3.14*(result[0]+isom)*250*250, m3)
     else:
         st.write('---')
         st.text("Predicted output:")
-        st.code(result[0]) 
+        st.code(3.14*(result[0])*250*250, m3) 
 else:
     st.write('---')
     st.text("Start-up Phase")
