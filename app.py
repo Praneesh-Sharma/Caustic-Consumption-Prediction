83bb918ab350691577de50717c04628649eef105
Praneesh Sharma <92104015+Praneesh-Sharma@users.noreply.github.com>
2023-06-21 15:04:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,15 +13,20 @@ nfccu = st.slider('NFCCU Feed Rate', 0.0, 220.0, 50.0)
 ofccu = st.slider('OFCCU Feed Rate', 0.0, 120.0,  50.0)
 ltu = st.slider('LTU Feed Rate', 0.0, 110.0,  50.0)  
 
-result = predict(np.array([[nfccu, ofccu, ltu]]))
-
-isom_chk = st.checkbox('Include ISOM Feed Rate')
-if isom_chk:
-    isom = st.number_input('Enter ISOM Consumption: ')
-    st.write('---')
-    st.text("Predicted output:")
-    st.code(result[0]+isom)
+if nfccu>80 and ofccu>80 and ltu>80:
+    result = predict(np.array([[nfccu, ofccu, ltu]]))
+    isom_chk = st.checkbox('Include ISOM Feed Rate')
+    if isom_chk:
+        isom = st.number_input('Enter ISOM Consumption: ')
+        st.write('---')
+        st.text("Predicted output:")
+        st.code(result[0]+isom)
+    else:
+        st.write('---')
+        st.text("Predicted output:")
+        st.code(result[0]) 
 else:
     st.write('---')
-    st.text("Predicted output:")
-    st.code(result[0])    
+    st.text("Start-up Phase")
+
+   
