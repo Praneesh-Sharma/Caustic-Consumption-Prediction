525b692b12cb66aaa2c62db50d1b7241134060c8
Praneesh Sharma <92104015+Praneesh-Sharma@users.noreply.github.com>
2023-07-06 13:52:45 +0530

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -1,5 +1,11 @@
 import joblib
 import pickle
+
+# def predict(data):
+#     clf = joblib.load("rf_model.sav")
+#     return clf.predict(data)
+
 def predict(data):
-    clf = joblib.load("rf_model.sav")
+    with open("consump_pred_v2.pkl", 'rb') as f:
+      clf = pickle.load(f)
     return clf.predict(data)
